ca859affd75d1d80703fc23cc41fb49cbb2c6180
Brad Morgan <jbradmorg16@gmail.com>
2024-01-29 06:25:23 -0500

    completed exercise

---
+++ For_Loops/natural_nums.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+2. Write a C program to compute the sum of the first 10 natural numbers.
+Expected Output :
+The first 10 natural number is :
+1 2 3 4 5 6 7 8 9 10
+The Sum is : 55
+*/
+
+int main() {
+
+    int num, sum = 0;
+
+    printf("Enter the number of digits: ");
+    scanf("%d", &num);
+
+    printf("\nThe first %d natural numbers: \n", num);
+
+    for (int i = 1; i <= num; i++) {
+        sum = sum + i;
+    
+    printf("%d ", i);
+    
+
+    }
+    
+    printf("\nSum: %d\n", sum);
+
+    return 0;
+}
--- Tutorials/dynamic_mem.c
+++ Tutorials/dynamic_mem.c
@@ -5,29 +5,29 @@ int main() {
     int num1;
     int* ptr;
 
-   printf("Enter the number of elements here: ");
-   scanf("%d", &num1);
-   printf("Number of elements entered: %d\n", num1);
+    printf("Enter size of the array: ");
+    scanf("%d", &num1);
+    printf("Size of array: %d values\n", num1);
 
-
-    ptr = (int*)malloc(num1 * sizeof(int));
+    ptr = (int*)malloc(5*sizeof(int)); // allocating memory 
 
     if (ptr == NULL) {
         printf("Memory not allocated...\n");
-        // exit(0);
-    }
-    else {
+        exit(0);
+    } else {
         printf("Memory successfully allocated...\n");
-    
-
-        for (int i = 0; i < num1; i++) {
+         for (int i = 0; i < num1; i++) {
             ptr[i] = i + 1;
-          }
+         }
 
-        printf("The elements in the array are: \n");
-        for(int i = 0; i < num1; i++) {
+         printf("The elements in the array are: %d\n", num1);
+         for (int i = 0; i < num1; i++) {
             printf("%d\n", ptr[i]);
+         }
+
     }
-    }
-        return 0;
+
+    free(ptr);
+
+    return 0;
 }
