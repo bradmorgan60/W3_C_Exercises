ee1aa90addf188d81887b636fffc522ecd25afb3
Brad Morgan <jbradmorg16@gmail.com>
2024-02-04 05:45:16 -0500

    updated Pointers exercise

--- Pointers/pointers_5.c
+++ Pointers/pointers_5.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
+/*
+5. Write a program in C to add numbers using call by reference.
+Test Data :
+Input the first number : 5
+Input the second number : 6
+Expected Output :
 
-long addNumbers(long *, long *);
+ The sum of 5 and 6  is 11 
+*/
+
+long Add2Numbers(long *, long *);
 
 int main() {
-    long fno, sno, sum;
-
+    long first, second, sum;
     printf("Enter the first number: ");
-    scanf("%ld", &fno);
+    scanf("%ld", &first);
 
     printf("Enter the second number: ");
-    scanf("%ld", &sno);
+    scanf("%ld", &second);
 
-    sum = addNumbers(&fno, &sno);
+    sum = Add2Numbers(&first, &second);
 
-    printf("Sum of %ld and %ld = %ld\n", fno, sno, sum);
-
+    printf("The sum of %ld and %ld is %ld\n", first, second, sum);
     return 0;
 }
 
-long addNumbers(long *n1, long *n2) {
+long Add2Numbers(long *n1, long *n2) {
     long sum;
     sum = *n1 + *n2;
     return sum;
