d6ff3f7fc295710ec1e9ac205ffa9b796f03c583
Brad Morgan <jbradmorg16@gmail.com>
2024-02-04 05:49:05 -0500

    updated

--- Pointers/pointers_5.c
+++ Pointers/pointers_5.c
@@ -10,9 +10,10 @@ Expected Output :
 */
 
 long Add2Numbers(long *, long *);
+long Mult2Numbers(long *, long *);
 
 int main() {
-    long first, second, sum;
+    long first, second, sum, product;
     printf("Enter the first number: ");
     scanf("%ld", &first);
 
@@ -20,8 +21,11 @@ int main() {
     scanf("%ld", &second);
 
     sum = Add2Numbers(&first, &second);
+    product = Mult2Numbers(&first, &second);
 
     printf("The sum of %ld and %ld is %ld\n", first, second, sum);
+    printf("The product of %ld and %ld is %ld\n", first, second, product);
+
     return 0;
 }
 
@@ -29,4 +33,10 @@ long Add2Numbers(long *n1, long *n2) {
     long sum;
     sum = *n1 + *n2;
     return sum;
+}
+
+long Mult2Numbers(long *n1, long *n2) {
+    long product;
+    product = *n1 * *n2;
+    return product;
 }
