1d3fe774ef02cc479ef211465d6d172a623e2d88
Brad Morgan <jbradmorg16@gmail.com>
2024-02-05 17:13:44 -0500

    performed exercise again w/ explanation

--- Pointers/pointers_4.c
+++ Pointers/pointers_4.c
@@ -11,21 +11,36 @@ The sum of the entered numbers is : 11
 */
 
 int main() {
-    int num1, num2, *first, *second, sum;
+    long num1, num2, *first, *second, sum;
 
-    printf("Adding two pointers together\n");
     printf("Enter the first integer: ");
-    scanf("%d", &num1);
+    scanf("%ld", &num1);
 
     printf("Enter the second integer: ");
-    scanf("%d", &num2);
+    scanf("%ld", &num2);
 
     first = &num1;
     second = &num2;
 
     sum = *first + *second;
 
-    printf("Sum: %d\n", sum);
+    printf("Sum: %ld\n", sum);
 
     return 0;
 }
+
+/*
+The purpose of this exercise is to practice how pointer variables interact with memory references
+
+We initialize 5 variables in the solution: num1, num2, *first, *second, sum
+- The requirement is to print the sum using pointers
+- The way we solve this is as follows:
+    - When we use scanf(), we are required to use & which acts as a reference in memory
+    - Pointers are also addresses in memory
+- With this logic, we can set the pointer values equal to the memory addresses from the input values
+- Those values, therefore, have the same memory addresses
+
+- Then, we are able add the pointers together. We cancel out the memory addresses and use the values stored in memory to achieve the sum.
+
+
+*/
