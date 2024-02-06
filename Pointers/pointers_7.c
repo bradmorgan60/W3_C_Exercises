9556c5d4edd18f6c5c35260547a8ee1342dab4ad
Brad Morgan <jbradmorg16@gmail.com>
2024-02-06 06:37:06 -0500

    completed

---
+++ Pointers/pointers_7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+7. Write a program in C to store n elements in an array and print the elements using a pointer.
+Test Data :
+Input the number of elements to store in the array :5
+Input 5 number of elements in the array :
+element - 0 : 5
+element - 1 : 7
+element - 2 : 2
+element - 3 : 9
+element - 4 : 8
+Expected Output :
+
+ The elements you entered are :                                                                               
+ element - 0 : 5                                                                                              
+ element - 1 : 7                                                                                              
+ element - 2 : 2                                                                                              
+ element - 3 : 9                                                                                              
+ element - 4 : 8   
+*/
+
+int main() {
+    int elements, arr1[25], *ptr;
+
+    printf("Input the number of elements to store in the array: ");
+    scanf("%d", &elements);
+
+    ptr = (int*)malloc(elements * sizeof(int));
+
+    printf("Input %d elements into the list: \n", elements);
+    for (int i = 0; i < elements; i++) {
+        printf("Element: %d : ", i);
+        scanf("%d", ptr + i);
+
+    }
+
+    printf("The %d elements you entered are: \n", elements);
+    for (int i = 0; i < elements; i++) {
+        printf("Element - %d : %d \n", i, *(ptr + i));
+    }
+
+    return 0;
+}
