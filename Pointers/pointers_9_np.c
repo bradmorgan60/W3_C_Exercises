e7c78948b21fd69b0ec3b904209e128da4784bbc
Brad Morgan <jbradmorg16@gmail.com>
2024-02-16 09:15:12 -0500

    completed

---
+++ Pointers/pointers_9_np.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int size, number[50];
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter the %d elements below: \n", size);
+
+    for (int i = 0; i < size; i++) {
+        printf("Enter an integer: %d : ", i + 1);
+        scanf("%d", &number[i]);
+    }
+
+    // store the largest number
+    for (int i = 0; i < size; i++) {
+        if (number[0] < number[i] ) {
+            number[0] = number[i];
+        }
+    }
+    
+
+    printf("The largest value : %d \n", number[0]);
+
+
+}
