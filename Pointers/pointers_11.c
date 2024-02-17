b2f95d647bcc19063556563d19f0e9741c50904e
Brad Morgan <jbradmorg16@gmail.com>
2024-02-17 12:34:01 -0500

    completed

---
+++ Pointers/pointers_11.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void SwapNums(int *ptr, int size);
+
+void SwapNums(int *ptr, int size) {
+    for (int i = 0; i < size / 2; i++) {
+        int temp = ptr[i];
+        ptr[i] = ptr[size - 1 - i];
+        ptr[size - 1 - i] = temp;
+    }
+}
+
+int main() {
+    int size, *ptr;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements below: \n");
+
+    ptr = calloc(size, sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        printf("Element-%d : ", i + 1);
+        scanf("%d", ptr + i);
+    }
+
+    printf("The value before swapping are: \n");
+    for (int i = 0; i < size; i++) {
+        printf("Element-%d = %d \n", ptr[i], *(ptr + i));
+    }
+
+    SwapNums(ptr, size);
+
+    printf("The value after swapping are: \n");
+    for (int i = 0; i < size; i++) {
+        printf("Element-%d = %d \n", ptr[i], *(ptr + i));
+    }
+
+    free(ptr); // use to prevent memory leaks
+
+    return 0;
+}
