c0bc06f0d4a1bca83c6e335c3d319841a4005a6c
Brad Morgan <jbradmorg16@gmail.com>
2024-02-17 13:08:32 -0500

    completed with comments

--- Pointers/pointers_11.c
+++ Pointers/pointers_11.c
@@ -4,12 +4,22 @@
 void SwapNums(int *ptr, int size);
 
 void SwapNums(int *ptr, int size) {
-    for (int i = 0; i < size / 2; i++) {
+    for (int i = 0; i < size / 2; i++) { // the reason to divide by 2 is to find the midpoint of the array
         int temp = ptr[i];
         ptr[i] = ptr[size - 1 - i];
         ptr[size - 1 - i] = temp;
     }
 }
+/*
+Logic for swap...
+Index:  0  1  2  3  4
+Array: [1, 2, 3, 4, 5]
+
+ptr[0] = temp;
+ptr[0] = ptr[5 - 1 - 0] (ptr[4])
+ptr[4] = temp;
+
+*/
 
 int main() {
     int size, *ptr;
@@ -17,7 +27,7 @@ int main() {
     printf("Enter the size of the array: ");
     scanf("%d", &size);
 
-    printf("Enter the elements below: \n");
+    printf("Enter the %d elements below: \n", size);
 
     ptr = calloc(size, sizeof(int));
 
