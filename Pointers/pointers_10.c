e9a7a763969d30368f6bba1620ee5af556793250
Brad Morgan <jbradmorg16@gmail.com>
2024-02-22 06:55:41 -0500

    optimized code

--- Pointers/pointers_10.c
+++ Pointers/pointers_10.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-
 /*
+C programming exercises_ Pointer - w3resource
 10. Write a program in C to calculate the length of a string using a pointer.
 Test Data :
 Input a string : w3resource
@@ -11,16 +9,31 @@ The length of the given string w3resource
 is : 10    
 */
 
+#include <stdio.h>
+int StringLength(char*);
+
 int main() {
-    char str[100];
+    char string[100];
+    int length;
 
-    printf("Enter a string: ");
-    scanf("%s", &str);
-    int x = strlen(str);
-    int *y;
-    y = &x;
-    printf("%d is the length of the string\n", *y);
+    printf("Program will ask for a string and display the length of that string\n");
+    printf("Enter the string below: ");
+    fgets(string, sizeof(string), stdin);
 
+    length = StringLength(string);
+
+    printf("\nThe length of '%s' : %d\n", string, length - 1);
 
     return 0;
+}
+
+int StringLength(char* character) {
+    int counter = 0;
+    while (*character != '\0')
+    {
+        counter++;
+        character++;
+    }
+    return counter;
+    
 }
