c561032e6e19541d0d30e31b500ae5586ada9c31
Brad Morgan <jbradmorg16@gmail.com>
2024-02-23 07:03:11 -0500

    updated

--- Structs/struct_pointer.c
+++ Structs/struct_pointer.c
@@ -2,30 +2,33 @@
 #include <stdlib.h>
 
 typedef struct {
-    int x;
-    int y;
+    int x, y;
+    char name[100];
+
 } Point;
 
 int main() {
     Point p1;
 
-    p1.x = 2;
-    p1.y = 4;
-
+    p1.x = 3;
+    p1.y = 5;
     printf("Sum: %d\n", p1.x + p1.y);
-
-    Point *p2; // assign a memory address to a struct
+    printf("Now let's work with pointers: \n");
+    Point *p2;
     p2 = &p1;
 
-    p2->x = 5;
-    p2->y = 4;
+    p2 -> x = 5;
+    p2 -> y = 10;
 
-    printf("Sum: %d\n", p1.x + p1.y);
+    printf("Sum: %d\n", p2->x + p2->y);
+    printf("Now we can use malloc to dynamically allocate space\n");
 
-    struct Point *p3;
+    Point *p3;
     p3 = malloc(sizeof(Point));
+    (*p3).x = 1;
+    (*p3).y = 2;
 
-
+    printf("Sum: %d\n", (*p3).x + (*p3).y);
 
     return 0;
 }
