7cabec99f83497642e21f22cef97d26f0105d48c
Brad Morgan <jbradmorg16@gmail.com>
2024-02-29 07:01:49 -0500

    added func

--- For_Loops/forLoops_exercises.c
+++ For_Loops/forLoops_exercises.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int sum = 0, num1;
-    printf("Please enter the number of values in the loop: ");
-    scanf("%d", &num1);
-    
-    for (int i = 1; i < num1 + 1; i++) {
-        printf("%d ", i);
-        sum += i;
+    int sum = 0, num1, *arr, size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    arr = calloc(size, sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        printf("Element-%d: ", i + 1);
+        scanf("%d", (arr + i));
     }
-    printf("\n");
+
+    printf("Array entered is below....\n");
+
+    for (int i = 0; i < size; i++) {
+        printf("Element-%d: %d\n", i + 1, *(arr + i));
+        sum += *(arr + i);
+    }
+
+    double average = sum / size;
 
     printf("Sum: %d\n", sum);
+    printf("Average: %lf\n", average);
+    
     return 0;
 }
