2dc55cb1635ab8ced54918d730678347d502b394
Brad Morgan <jbradmorg16@gmail.com>
2024-02-29 07:11:28 -0500

    completed

---
+++ For_Loops/cube.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+. Write a program in C to display the cube of the number up to an integer.
+Test Data :
+Input number of terms : 5
+Expected Output :
+Number is : 1 and cube of the 1 is :1
+Number is : 2 and cube of the 2 is :8
+Number is : 3 and cube of the 3 is :27
+Number is : 4 and cube of the 4 is :64
+Number is : 5 and cube of the 5 is :125
+*/
+
+int main() {
+    int num;
+    printf("Enter the number of terms: ");
+    scanf("%d", &num);
+
+    for (int i = 0; i < num; i++) {
+        printf("Number is : %d and the cube of %d is %d\n", i + 1, i + 1, (i + 1)*(i+1)*(i+1));
+    }
+
+    return 0;
+}
