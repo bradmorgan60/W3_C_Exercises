d43f784dfe39ea1bb5c32249dc07fb259f847f7a
Brad Morgan <jbradmorg16@gmail.com>
2024-03-01 06:38:26 -0500

    updated, completed

--- For_Loops/forLoops_11.c
+++ For_Loops/forLoops_11.c
@@ -22,5 +22,13 @@ int main() {
 
     }
 
+    printf("-----------\n");
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++)
+            printf("%d", j);
+        printf("\n");
+    }
+
     return 0;
 }
