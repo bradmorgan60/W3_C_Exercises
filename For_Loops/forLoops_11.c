48f4dfa88346ada52b174da5f6b3b9b53080cba3
Brad Morgan <jbradmorg16@gmail.com>
2024-03-01 06:35:21 -0500

    completed

---
+++ For_Loops/forLoops_11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+11. Write a program in C to make such a pattern like a right angle triangle with a number which will repeat a number in a row.
+
+The pattern like :
+
+ 1
+ 22
+ 333
+ 4444
+*/
+
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++)
+            printf("%d", i);
+        printf("\n");
+
+    }
+
+    return 0;
+}
