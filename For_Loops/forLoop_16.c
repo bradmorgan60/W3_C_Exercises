d7d07070a4a0b4620ba5c8e95f31a26fc67e8070
Brad Morgan <jbradmorg16@gmail.com>
2024-03-03 07:04:52 -0500

    completed

---
+++ For_Loops/forLoop_16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+16. Write a C program to display the sum of n terms of even natural numbers.
+Test Data :
+Input number of terms : 5
+Expected Output :
+The even numbers are :2 4 6 8 10
+The Sum of even Natural Number upto 5 terms : 30
+*/
+
+int main() {
+    int terms, nums, sum = 0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+
+    printf("\nEven numbers: ");
+    for (int i = 1; i <= terms; i++) {
+        nums = i * 2;
+        printf("%d ", nums);
+        sum += nums;
+    }
+    printf("\n-----\n");
+    printf("\nSum of even natural numbers up to %d terms: %d\n", terms, sum);
+
+    return 0;
+}
+
