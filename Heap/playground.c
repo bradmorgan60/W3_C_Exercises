1d30a04d6393d74ab9f0b42a54717cfe9657303f
Brad Morgan <jbradmorg16@gmail.com>
2024-03-09 06:45:15 -0500

    redoing exercise 1

--- Heap/playground.c
+++ Heap/playground.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +6,7 @@
 
 struct Heap {
     int size;
-    int arr[MAX_HEAP_SIZE];
+    int arr[];
 };
 
 void swap(int *n1, int *n2) {
@@ -14,36 +15,88 @@ void swap(int *n1, int *n2) {
     *n2 = temp;
 }
 
+// Root should be the max
 void HeapifyDown(struct Heap *heap, int index) {
-    // index should be the largest value...now we try to 
     int largest = index;
     int leftChild = 2 * index + 1;
-    int RightChild = 2 * index + 2;
+    int rightChild = 2 * index + 2;
 
-    if (index != largest)
+    if (leftChild < heap->size && heap->arr[leftChild] > heap->arr[largest])
     {
-        swap(heap->arr[index], heap->arr[largest]);
+        largest = leftChild;
     }
-    
+    else if (rightChild < heap->size && heap->arr[rightChild] > heap->arr[largest]) {
+        largest = rightChild;
+    }
+    else if(index != largest) {
+        swap(&heap->arr[index], &heap->arr[largest]);
+        HeapifyDown(heap, largest);
+    }
 }
 
+// root should be the min
 void HeapifyUp(struct Heap *heap, int index) {
     int smallest = index;
     int leftChild = 2 * index + 1;
     int rightChild = 2 * index + 2;
 
-    if (index != smallest) { // call the swap function
-        swap(heap->arr[index], heap->arr[smallest]);
+    if (leftChild < heap->size && heap->arr[leftChild] < heap->arr[smallest])
+    {
+        smallest = leftChild;
     }
+    else if (rightChild < heap->size && heap->arr[rightChild] < heap->arr[smallest]) {
+        smallest = rightChild;
+    }
+    else if(index != smallest) {
+        swap(&heap->arr[index], &heap->arr[smallest]);
+        HeapifyUp(heap, smallest);
+    }
+    
+}
+
+void insert(struct Heap *heap, int key) {   // Key could be any integer value 
+    if (heap->size >= MAX_HEAP_SIZE)
+    {
+        printf("Heap overflow...");
+        return;
+    }
+
+    heap->arr[heap->size] = key;
+    heap->size++;
+
+    HeapifyUp(heap, heap->size - 1);
+
+
+}
+
+int deleteMax(struct Heap *heap) {
+    if (heap->size <= 0) {
+        printf("Heap underflow...");
+        return -1;
+    }
+
+    int maxElement = heap->arr[0];
+    heap->arr[0] = heap->arr[heap->size - 1];
+    heap->size--;
+
+    HeapifyDown(heap, 0);
+    return maxElement;
 }
 
 void display(struct Heap *heap) {
-    printf("Heap elements: ");
+    printf("Array: ");
     for (int i = 0; i < heap->size; i++) {
         printf("%d ", heap->arr[i]);
     }
 }
 
+
+
 int main() {
+    struct Heap heap;
+    heap.size = 100;
+
+    display(&heap);
+
     return 0;
 }
