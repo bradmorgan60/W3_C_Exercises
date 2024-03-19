ca7c0940b66a5b37c3757917fadbf3696790c894
Brad Morgan <jbradmorg16@gmail.com>
2024-03-19 07:19:31 -0400

    working progress

--- Callback_Function/exercise2.c
+++ Callback_Function/exercise2.c
@@ -1,51 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int compare(int x, int y) {
+    if (x > y) return 1;
+    else return -1;
 
-int my_compare(int a, int b) {
-    if (a > b) return -1;
-    else return 1;
 }
-
-// Bubble sort algorithm
 
 void BubbleSort(int size, int *arr, int (*compare)(int, int)) {
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (my_compare(arr[i], arr[j] > 0)) {
-                int temp = arr[i];
+    int temp;
+    for (int i = 0; i < size; i++) 
+        for (int j = i + 1; i < size; i++) {
+            if (compare(arr[i], arr[j]) > 0) {
+                temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
-        }
-        printf("%d ", arr[i]);
-    }
-}
-
-void Descending(int size, int *arr) {
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (arr[i] < arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        printf("%d ", arr[i]);
-    }
-}
-
-// Bubble sort algorithm
-void Ascending(int size, int *arr) {
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (arr[i] > arr[j]) {     // The only difference with the above function is logic operator "<" vs ">"
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        printf("%d ", arr[i]);
+        // printf("%d ", arr[i + 1]);
     }
 }
 
@@ -60,23 +31,18 @@ int main() {
         scanf("%d", (arr + i));
     }
 
-    printf("----------------\n");
-    printf("Expected Output: \n");
-    printf("Original array: ");
-
-    for (int i = 0; i < size; i++) { 
-        printf("%d ", *(arr + i)); 
-        }
-    // printf("\n");
-    // BubbleSort(size, arr, my_compare);
-
-    printf("\nAscending order: ");
-    Ascending(size, arr);
-
-    printf("\nDescending order: ");
-    Descending(size, arr);
-
+    printf("Original Array: \n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
+    }
     printf("\n");
+
+    printf("Sorted array...\n");
+    BubbleSort(size, arr, compare);
+    for (int i = 0; i < size; i++){
+        printf("%d ", *(arr + i));
+    }
+
 
     return 0;
 }
--- Callback_Function/exercise2_solution.c
+++ Callback_Function/exercise2_solution.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Callback function fundamentals
 void Apple() {
     printf("Apple");
 }
