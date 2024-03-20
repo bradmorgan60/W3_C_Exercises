ab761e9452d23fd3b0bf9acb78b0c652d545d01f
Brad Morgan <jbradmorg16@gmail.com>
2024-03-20 06:35:41 -0400

    completed

--- Callback_Function/exercise2.c
+++ Callback_Function/exercise2.c
@@ -1,23 +1,39 @@
+/*
+2. Write C program to sort an array of integers in ascending or descending order using a callback function to compare the elements.
+Expected Output:
+
+Original array: 7 2 0 5 8 9 
+
+Ascending order: 0 2 5 7 8 9 
+
+Descending order: 9 8 7 5 2 0
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int compare(int x, int y) {
-    if (x > y) return 1;
-    else return -1;
-
+    if (x > y) return -1;
+    else return 1;
 }
 
-void BubbleSort(int size, int *arr, int (*compare)(int, int)) {
+// Bubble sort algorithm - use a nested for loop to compare indexes side-by-side
+void Bubble_Sort(int size, int *arr) {
     int temp;
-    for (int i = 0; i < size; i++) 
-        for (int j = i + 1; i < size; i++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
             if (compare(arr[i], arr[j]) > 0) {
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
-        // printf("%d ", arr[i + 1]);
+
+            // printf("%d ", arr[i]);
+        }
+        printf("%d ", arr[i]);
     }
+    printf("\n");
+        
 }
 
 int main() {
@@ -26,23 +42,24 @@ int main() {
     scanf("%d", &size);
 
     arr = calloc(size, sizeof(int));
+
+    // User inputs all elements in array based on size selected
     for (int i = 0; i < size; i++) {
         printf("Element-%d: ", i + 1);
-        scanf("%d", (arr + i));
+        scanf("%d", arr + i);
     }
 
-    printf("Original Array: \n");
+    // print all integers that were entered...
+    printf("Original Array: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", *(arr + i));
     }
+
     printf("\n");
 
-    printf("Sorted array...\n");
-    BubbleSort(size, arr, compare);
-    for (int i = 0; i < size; i++){
-        printf("%d ", *(arr + i));
-    }
-
+    printf("Ascending Order: ");
+    Bubble_Sort(size, arr);
+    
 
     return 0;
 }
