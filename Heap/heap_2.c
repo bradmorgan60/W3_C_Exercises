dd480ea7a2e5b7b6e357213854df3aa1450a2ac0
Brad Morgan <jbradmorg16@gmail.com>
2024-03-28 06:41:19 -0400

    exercise redone

--- Heap/heap_2.c
+++ Heap/heap_2.c
@@ -1,99 +1,88 @@
-#include <stdio.h> // library for input / output for printf function
-#include <stdlib.h> // library for dynamic memory allocation
+#include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_HEAP_SIZE 100 // define the max size of the heap
+#define MAX_HEAP_SIZE 100
 
-// Function used to swap two elements in the heap
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *num1, int *num2) {
+    int temp;
+    temp = *num1;
+    *num1 = *num2; 
+    *num2 = temp;
 }
 
-// Maintain the max heap property after an element is inserted
-void heapifyDown(int arr[], int size, int index) {
+void HeapifyDown(int index, int size, int *arr) {
+    int largest = index;
     int leftChild = 2 * index + 1;
     int rightChild = 2 * index + 2;
-    int largest = index;
-    //
 
-    if (leftChild < size && arr[leftChild] > arr[largest])
-    {
-        largest = leftChild;
+    if (arr[leftChild] < size && arr[leftChild] > arr[largest]) {
+        leftChild = largest;
     }
 
-    if (rightChild < size && arr[rightChild] > arr[largest])
-    {
-        largest = rightChild;
+    if (arr[rightChild] < size && arr[rightChild] > arr[largest]) {
+        rightChild = largest;
     }
 
-    if (largest != index)
-    {
+    if (index != largest) {
         swap(&arr[index], &arr[largest]);
-        heapifyDown(arr, size, largest);
+        HeapifyDown(largest, size, arr);
     }
 }
 
-void buildMaxHeap(int arr[], int size) {
-    for (int i = (size / 2) - 1; i >= 0; i--) {
-        heapifyDown(arr, size, i);
-    }
+int compare(int x, int y) {
+    if (x > y) return 1;
+    else return -1;
 }
 
-void display(int arr[], int size) {
-    printf("Array elements: ");
+void sort(int *arr, int size) {
+    int temp;
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (compare(arr[i], arr[j]) > 0) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void display(int *arr, int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
+void BuildMaxHeap(int size, int *arr) {
+    for (int i = (size / 2) - 1; i >= 0; i--) {
+        HeapifyDown(i, size, arr);
+    }
+    
+}
+
 int main() {
-    int size, *arr;
-    // Test with a random array
-    printf("Enter the size of the array: ");
+    int size;
+    int *arr = calloc(size, sizeof(int));
+
+    printf("Enter size of array: ");
     scanf("%d", &size);
 
-    printf("Cool, now enter the %d elements below: \n", size);
-
-    arr = calloc(size, sizeof(int));
-
-    // user inputs the array
     for (int i = 0; i < size; i++) {
-        printf("Element-%d: ", i + 1);
-        scanf("%d", (arr + i));
-    }
-    printf("\n----------\n");
-    for (int i = 0; i < size; i++) {
-        printf("Element-%d: %d\n", (i + 1), *(arr + i));
+        printf("Num-%d: ", i + 1);
+        scanf("%d", arr + i);
     }
 
-    // int arr[] = {6, 8, 12, 7, 1};
-    int randomSize = sizeof(&arr) / sizeof(&arr[0]); // integer = 4 bytes
-    // printf("%d \n", randomSize);
-
-    printf("Original Random Array: \n");
+    printf("Original array: ");
     display(arr, size);
+    printf("Sorted array: ");
+    sort(arr, size);
 
-    buildMaxHeap(arr, size);
-
-    printf("\n");
-    printf("Max Heap of Random Array: \n");
+    printf("Max heap: ");
+    BuildMaxHeap(size, arr);
     display(arr, size);
-
-    // Test with sorted array
-    int sortedArray[] = {19, 12, 8, 6, 2};
-
-
-    int sortedSize = sizeof(sortedArray) / sizeof(sortedArray[0]);
-
-    printf("\nOriginal Sorted Array: \n");
-    display(sortedArray, sortedSize);
-
-    buildMaxHeap(sortedArray, sortedSize);
-
-    printf("\nMax Heap from Sorted Array:\n");
-    display(sortedArray, sortedSize);
 
 
     return 0;
