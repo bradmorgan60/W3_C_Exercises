7e3f72ea0b23810cee31f3a36f05ea2bde2564bb
Brad Morgan <jbradmorg16@gmail.com>
2024-03-31 11:18:58 -0400

    progressing

--- Queue/exercise1.c
+++ Queue/exercise1.c
@@ -22,22 +22,29 @@ Check the queue is empty or not? No
 #include <stdio.h>
 #include <stdlib.h>
 
-void display(int size, int *arr) {
+#define MAX_HEAP_SIZE 100
+
+struct Heap {
+    int size;
+    int arr[MAX_HEAP_SIZE];
+};
+
+void display(struct Heap *heap) {
     printf("Queue elements are: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+    for (int i = 0; i < heap->size; i++) {
+        printf("%d ", *(heap->arr + i));
     }
     printf("\n");
 }
 
-void insert(int size, int *arr, int key) {
-    arr[size] = key;
-    size++;
+void insert(struct Heap *heap, int key) {
+    heap->arr[heap->size] = key;
+    heap->size++;
 }
 
-void is_empty(int size, int *arr) {
-    for (int i = 0; i < size; i++) {
-        if (arr == NULL) {
+void is_empty(struct Heap *heap) {
+    for (int i = 0; i < heap->size; i++) {
+        if (heap->arr == NULL) {
             printf("Array is empty...\n");
             break;
         } else {
@@ -47,31 +54,14 @@ void is_empty(int size, int *arr) {
     }
 }
 
-int main() {
-    int size, *arr;
-
-    is_empty(size, arr);
-
+void User_Input(struct Heap *heap) {
     printf("Initialize the queue!\n");
-
-    printf("Enter the size of the queue: ");
-    scanf("%d", &size);
-
-    arr = malloc(size * sizeof(int));
-
-    for (int i = 0; i < size; i++) {
-        printf("Num-%d: ", i + 1);
-        scanf("%d", (arr + i));
-    }
-    // printf("\n");
-
-    is_empty(size, arr);
-    display(size, arr);
-
-
     
 
+}
 
-
+int main() {
+    struct Heap heap;    
+    // printf("\n");
     return 0;
 }
