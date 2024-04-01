eb49bf4f5feb8aff74c1f4026bdd7d769a2e37d1
Brad Morgan <jbradmorg16@gmail.com>
2024-04-01 06:50:41 -0400

    redoing exercise

--- Queue/exercise1.c
+++ Queue/exercise1.c
@@ -1,8 +1,8 @@
 /*
-1. Write a C program to implement a queue using an array. Programs should contain functions for 
-  inserting elements into the queue
-  displaying queue elements
-  checking whether the queue is empty or not
+. Write a C program to implement a queue using an array. Programs should contain functions for... 
+    1. Inserting elements into the queue 
+    2. Displaying queue elements
+    3. Checking whether the queue is empty or not
 
 Expected Output:
 
@@ -10,53 +10,43 @@ Initialize a queue!
 Check the queue is empty or not? Yes
 
 Insert some elements into the queue:
-Queue elements are: 1 2 3 
+Queue elements are: 1 2 3
 
 Insert another element into the queue:
-Queue elements are: 1 2 3 4 
+Queue elements are: 1 2 3 4
 
-Check the queue is empty or not? No  
+Check the queue is empty or not? No
 */
-
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAX_HEAP_SIZE 100
 
 struct Heap {
     int size;
     int *arr;
 };
 
-void display(struct Heap *heap) {
-    printf("Original array: ");
-    for (int i = 0; i < heap->size; i++) {
-        printf("%d ", *(heap->arr + i));
+void display(int size, int *arr) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
     }
     printf("\n");
 }
 
-void User_Input(struct Heap *heap) {
-    printf("Initialize the queue!\n");
-    printf("Enter the size of the queue: ");
-    scanf("%d", &heap->size);
-
-    for (int i = 0; i < heap->size; i++) {
-        printf("Num-%d: ", (i + 1));
-        scanf("%d", (heap->arr + i));
-    }
-}
-
-int is_empty(struct Heap *heap) {
-    if (heap->arr == NULL) return 1;
+int is_empty(int size, int *arr) {
+    struct Heap *heap;
+    if (sizeof(heap->arr) == 0) return 1;
     else return -1;
 }
+
 
 int main() {
     struct Heap *heap;
     heap->arr = calloc(heap->size, sizeof(int));
-    User_Input(heap);
-    display(heap);
+
+    
+
     return 0;
 }
+
+
