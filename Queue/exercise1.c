e0dcbc5920b7fffd45cb0e085c68912187a71afb
Brad Morgan <jbradmorg16@gmail.com>
2024-04-02 09:40:04 -0400

    progressing

--- Queue/exercise1.c
+++ Queue/exercise1.c
@@ -33,18 +33,31 @@ void display(int size, int *arr) {
     printf("\n");
 }
 
-int is_empty(int size, int *arr) {
+void is_empty(int size, int *arr) {
     struct Heap *heap;
-    if (sizeof(heap->arr) == 0) return 1;
-    else return -1;
+    if (sizeof(heap->arr) <= 7) {
+        printf("Array is empty\n");
+    } else {
+        printf("Array is not empty\n");
+    }
 }
 
 
 int main() {
     struct Heap *heap;
+    is_empty(heap->size, heap->arr);
+
+    printf("Enter size of queue: ");
+    scanf("%d", &heap->size);
     heap->arr = calloc(heap->size, sizeof(int));
 
-    
+    for (int i = 0; i < heap->size; i++) {
+        printf("Num-%d: ", i + 1);
+        scanf("%d", (heap->arr + i));
+    }
+
+    display(heap->size, heap->arr);
+    is_empty(heap->size, heap->arr);
 
     return 0;
 }
