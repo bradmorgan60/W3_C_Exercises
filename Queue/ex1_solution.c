33e0caef66821f1d9625abfd4c3958332b63cc77
Brad Morgan <jbradmorg16@gmail.com>
2024-04-03 05:54:32 -0400

    added solution

---
+++ Queue/ex1_solution.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+// define global variables
+int queue[MAX_SIZE];    // define max size of queue
+int front = -1; // initialize front of queue
+int back = -1; // initialize back of queue
+
+
+int is_empty() {
+    if (front == -1 || front > back) {
+        return 1;
+    }
+    return 0; // return 0 if queue is NOT empty
+
+}
+
+void enqueue(int item) {
+    if (back == MAX_SIZE - 1) {
+        printf("Error: Queue is full\n");
+        return;
+    }
+    if (front == -1) {
+        front = 0;
+    }
+
+    back++;
+    queue[back] = item;
+
+}
+
+void display() {
+    if (front == -1 || front > back) {
+        printf("Queue is empty...\n");
+        return;
+    }
+    printf("Queue elements are: ");
+    for (int i = front; i <= back; i++) {
+        printf("%d ", queue[i]);
+    }
+
+    printf("\n");
+
+}
+
+int main() {
+    printf("Initialize the queue!\n");
+    printf("Check if the queue is empty or not? %s\n", is_empty() ? "Yes" : "No");
+    printf("\n");
+    
+    printf("Insert some elements into the queue: \n");
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+
+    display();
+    printf("Insert another element into the queue: \n");
+    enqueue(4);
+    display();
+
+    printf("Check if the queue is empty or not? %s\n", is_empty() ? "Yes" : "No");
+
+
+    return 0;
+}
--- Queue/exercise1.c
+++ Queue/exercise1.c
@@ -1,65 +1,19 @@
-/*
-. Write a C program to implement a queue using an array. Programs should contain functions for... 
-    1. Inserting elements into the queue 
-    2. Displaying queue elements
-    3. Checking whether the queue is empty or not
-
-Expected Output:
-
-Initialize a queue!
-Check the queue is empty or not? Yes
-
-Insert some elements into the queue:
-Queue elements are: 1 2 3
-
-Insert another element into the queue:
-Queue elements are: 1 2 3 4
-
-Check the queue is empty or not? No
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Heap {
-    int size;
-    int *arr;
-};
+#define MAX_SIZE 100
 
-void display(int size, int *arr) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
-    }
-    printf("\n");
+
+int back = -1;
+int front = -1;
+int queue[MAX_SIZE];
+
+void display() {
+    
 }
-
-void is_empty(int size, int *arr) {
-    struct Heap *heap;
-    if (sizeof(heap->arr) <= 7) {
-        printf("Array is empty\n");
-    } else {
-        printf("Array is not empty\n");
-    }
-}
-
 
 int main() {
-    struct Heap *heap;
-    is_empty(heap->size, heap->arr);
-
-    printf("Enter size of queue: ");
-    scanf("%d", &heap->size);
-    heap->arr = calloc(heap->size, sizeof(int));
-
-    for (int i = 0; i < heap->size; i++) {
-        printf("Num-%d: ", i + 1);
-        scanf("%d", (heap->arr + i));
-    }
-
-    display(heap->size, heap->arr);
-    is_empty(heap->size, heap->arr);
-
     return 0;
 }
-
 
