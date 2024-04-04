e46f9f7421d177c9b46f7076a736b554c1a20460
Brad Morgan <jbradmorg16@gmail.com>
2024-04-04 08:49:03 -0400

    my own solution

--- Queue/exercise1.c
+++ Queue/exercise1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 100
 // need to set some global variables before we put these into the functions
@@ -9,38 +10,57 @@ int front = -1;
 int back = -1;
 int queue[MAX_SIZE];
 
-// function to insert elements into the queue
-void enqueue() {
-    if (back = MAX_SIZE - 1) {
+// function to insert elements into the queue...will eventually pass into a for loop for user input
+void enqueue(int item) {
+    if (back == MAX_SIZE - 1) {
         printf("Queue is full\n");
         return;
     } 
-    if (front = -1) {
+    if (front == -1) {
         front = 0;
     }
-
+    back++;
+    queue[back] = item;
 }
 
 // function to display the current queue
 void display() {
     int size;
-    printf("Current queue: ");
-    for (int i = 0; i < size; i++) {
-        printf("Element-%d: ", i + 1);
-    }
+    printf("Enter the size of the queue: ");
+    scanf("%d", &size);
 
+    for (int i = 0; i < size; i++) {
+        int number;
+        printf("Element-%d: ", i + 1);
+        scanf("%d", &number);
+        enqueue(number);
+    }
+    printf("Queue elements: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
 }
 
 // function to check if the queue is empty or not
 int is_empty() {
-    if (front = -1 || front > back) {
+    if (front == -1 || front > back) {
         return 1;
     }
     return 0;
 }
 
 int main() {
-    printf("Initialize the queue!\n");
+    printf("Empty queue? %s\n", is_empty() ? "Yes":"No \n");
+
+    if (is_empty() == true) {
+        printf("Initialize the queue!\n");
+        display();
+    }
+    
+    printf("Empty queue? %s\n", is_empty() ? "Yes":"No \n");
+
+
 
     return 0;
 }
