e7e80c166c827f173e90e9fea7f38888eb2dd20e
Brad Morgan <jbradmorg16@gmail.com>
2024-04-08 18:21:52 -0400

    completed exercise...easily

--- Queue/exercise1.c
+++ Queue/exercise1.c
@@ -24,6 +24,7 @@ int back = -1;
 int queue[MAX_SIZE];
 
 void display() {
+    printf("Queue: ");
     for (int i = front; i <=back; i++) {
         printf("%d ", queue[i]);
     }
@@ -40,11 +41,34 @@ void enqueue(int number) {
 }
 
 int is_empty() {
-    if (front == 1 || front > back) return 1;
+    if (front == -1 || front > back) return 1;
     else return 0;
 }
 
 int main() {
-    if()
+    if(is_empty() == true) {
+        printf("Initialize the queue!\n");
+        printf("Check if the queue is empty or not: %s", is_empty() ? "Yes\n":"No\n");
+
+        printf("Enter size of queue: ");
+        int size;
+        scanf("%d", &size);
+        for (int i = 0; i < size; i++) {
+            int number;
+            printf("Num-%d: ", i + 1);
+            scanf("%d", &number);
+            enqueue(number);
+        }
+    
+    display();
+    printf("Inserting %d to the queue: ", 5);
+    enqueue(5);
+
+    printf("\n");
+
+    display();
+
+
+    }
     return 0;
 }
