702bf578dbeb55025e34eebb763ee1ec8e8ed96f
Brad Morgan <jbradmorg16@gmail.com>
2024-04-13 11:23:59 -0400

    working code

--- Queue/exercise6.c
+++ Queue/exercise6.c
@@ -25,18 +25,6 @@ typedef struct {
     int queue[MAX_SIZE];
 } Queue;
 
-void enqueue(Queue* q, int number) {
-    if (q->back == MAX_SIZE - 1) {
-        printf("Queue is full. No more integers may be added\n");
-        return;
-    }
-    if (q->front == -1 || q->front > q->back) {
-        q->front = 0;
-    }
-
-    q->back++;
-    q->queue[q->back] = number;
-}
 
 Queue* create_queue() {
     int size;
@@ -48,11 +36,70 @@ Queue* create_queue() {
 }
 
 void display(Queue* q) {
-    printf("Queue: ");
+    // printf("Queue: ");
     for (int i = q->front; i <= q->back; i++) {
         printf("%d ", q->queue[i]);
     }
     printf("\n");
+}
+
+int isEmpty(Queue* q) {
+    return q->back == -1;  // -1 indicates an empty queue
+}
+
+int isFull (Queue* q) {
+    return q->back == MAX_SIZE -1; // full queue
+}
+
+void enqueue(Queue* q, int number) {
+    if (isFull(q)) {
+        printf("Queue is full\n");
+    } else {
+        if (q->front == -1) {
+            q->front = 0;
+        }
+        q->back++;
+        q->queue[q->back] = number;
+    }
+
+}
+
+int dequeue(Queue* q) {
+    int item;
+    if (isFull(q)) {
+        printf("Queue is full\n");
+        item = -1;
+    }
+    else {
+        item = q->queue[q->front];
+        q->front++;
+        if (q->front > q->back) {
+            q->front = q->back = -1;
+        }
+    }
+    return item;
+}
+
+void reverse(Queue* q) {
+    /*
+        Replace index 0 with index -1? 
+        Would this just include a swap algorithm?
+
+        Need to dequeue everything, store in a temporary array, then enqueue them
+        LIFO data structure...which is how the stack works
+    */
+   int temp[MAX_SIZE];
+   int top = -1;
+   while (!isEmpty(q)) {
+        temp[++top] = dequeue(q);
+   }
+   // dequeue EVERYTHING from the current queue and store in a temporary data structure
+
+   while (top != -1) {
+        enqueue(q, temp[top--]);
+    // queue EVERYTHING back into reverse order
+   }
+
 }
 
 int main() {
@@ -68,6 +115,21 @@ int main() {
         enqueue(q, number);
     }
 
+    printf("Queue: ");
+    display(q);
+    reverse(q);
+
+    printf("Reversed queue: ");
+    display(q);
+
+    enqueue(q, 100);
+    enqueue(q, 200);
+
+    printf("New values: ");
+    display(q);
+
+    printf("Reverse again: ");
+    reverse(q);
     display(q);
 
 
