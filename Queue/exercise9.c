6e8ed78da4d1c4bf3305a56d74bdc06efe5ec2f6
Brad Morgan <jbradmorg16@gmail.com>
2024-04-17 08:02:59 -0400

    redoing exercise

--- Queue/exercise9.c
+++ Queue/exercise9.c
@@ -22,20 +22,15 @@ typedef struct {
     int front;
     int back;
     int queue[MAX_SIZE];
+
 } Queue;
 
 Queue* create_queue() {
     Queue* q = (Queue*)malloc(sizeof(Queue));
     q->front = -1;
     q->back = -1;
-    return q;
-}
 
-void display(Queue* q) {
-    for (int i = q->front; i <= q->back; i++) {
-        printf("%d ", i + 1);
-    }
-    printf("\n");
+    return q;
 }
 
 int is_empty(Queue* q) {
@@ -46,9 +41,31 @@ int is_full(Queue* q) {
     return q->back == MAX_SIZE - 1;
 }
 
+int max(Queue* q) {
+    int max = q->queue[q->front];
+    if (is_empty(q)) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    for (int i = q->front; i <= q->back; i++) {
+        if (q->queue[i] > max) {
+            max = q->queue[i];
+        }
+    }
+    return max;
+}
+
+void display(Queue* q) {
+    for (int i = q->front; i <= q->back; i++) {
+        printf("%d ", q->queue[i]);
+    }
+    printf("\n");
+}
+
 void enqueue(Queue* q, int number) {
     if (is_full(q)) {
-        printf("Queue is full\n");
+        printf("Queue is full...no more values may be added\n");
         return;
     }
 
@@ -60,69 +77,8 @@ void enqueue(Queue* q, int number) {
     q->queue[q->back] = number;
 }
 
-void dequeue(Queue* q) {
-    if (is_empty(q)) {
-        printf("Queue is empty...\n");
-        return;
-    }
-    q->front++;
-}
-
-int max(Queue* q) {
-    int max = q->queue[q->front];
-    if (q->front == -1 || q->front > q->back) {
-        printf("Queue is empty\n");
-    }
-    for (int i = q->front; i <= q->back; i++) {
-        if (q->queue[i] > max) {
-            max = q->queue[i];
-        }
-    }
-    return max;
-
-}
-
-
 int main() {
-    int size, number;
-
     Queue* q = create_queue();
 
-    if (is_empty(q)) {
-
-        printf("Initialize the queue!\n");
-        printf("Enter the size of the queue: ");
-        scanf("%d", &size);
-        for (int i = 0; i < size; i++) {
-            printf("Num-%d: ", i + 1);
-            scanf("%d", &number);
-            enqueue(q, number);
-        }
-}
-        printf("Queue: ");
-        display(q);
-
-
-        int isMax = max(q);
-        printf("Max value: %d\n", isMax);
-
-
-        printf("Add 3 more elements to the queue: \n");
-        enqueue(q, 300);
-        enqueue(q, 400);
-
-        isMax = max(q);
-        printf("Max value: %d\n", isMax);
-
-
-        printf("Remove 3 more elements to the queue: \n");
-        dequeue(q);
-        dequeue(q);
-        dequeue(q);
-
-        isMax = max(q);
-        printf("Max value: %d\n", isMax);
-
-
-return 0;
+    return 0;
 }
