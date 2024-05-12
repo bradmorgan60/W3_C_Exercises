64215f1d253f05c35e17d3d6ee59386695578dac
Brad Morgan <jbradmorg16@gmail.com>
2024-05-12 10:14:54 -0500

    beginning

---
+++ Tree/exercise1.c
@@ -0,0 +1,19 @@
+/*
+1. Write a C program that creates a binary tree. 
+   Allow users to input nodes and build a binary tree structure.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int data;
+    int TreeNode* left;
+    int TreeNode* right;
+}
+
+int main() {
+    int leftNode, rightNode, index;
+
+     return 0;
+}
