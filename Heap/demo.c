1730058013763937d9985892ba12b1ea12a45099
Brad Morgan <jbradmorg16@gmail.com>
2024-05-29 16:21:48 +0300

    progressing

--- Heap/demo.c
+++ Heap/demo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_HEAP_SIZE 100
 
 /*
 1. Write a C program that implements the basic operations of a heap - insert, delete, and display.
@@ -9,9 +10,8 @@
 // we can also visualize this in the form of an array
 
 struct Heap {
-    int leftNode;
-    int rightNode;
-    int data;
+   int arr[MAX_HEAP_SIZE];
+   int size;
 };
 
 void display(int size, int *arr) {
@@ -25,36 +25,37 @@ int compare(int a, int b) {
     return a > b;
 }
 
-void swap(int size, int *arr) {
-    // We want to have the largest node as the root. The user will input the values
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (compare(arr[i], arr[j])) {
-                int temp;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
+void swap(int num1, int num2) {
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
 }
 
 // 
-void HeapifyUp(struct Heap) {
-    struct Heap heap;
-    /* 
-        If the new value is less than the root, then that value will become the new root
-        The root value must be maintained if the new value is greater than the root
-    */
-   if (heap->leftNode < heap->data) {
-        
+void HeapifyDown(struct Heap *heap, int index) {
+    int largest = index;
+    int leftChild = 2 * index + 1;
+    int rightChild = 2 * index + 2;
 
-   }
+    if (heap->arr[leftChild] > heap->arr[largest])
+        {
+            largest = leftChild;
+        }
+    
+    if (heap->arr[rightChild] > heap->arr[largest])
+        {
+            largest = rightChild;
+        }
+    
+    if (index != largest) {
+        swap(heap->arr[index], heap->arr[largest]);
+        HeapifyDown(heap, largest);
+    }
 
 }
 
 int main() {
-    struct Heap;
+    struct Heap heap;
     int size, *arr;
     printf("Demo environment for working with Heap data structure\n");
     printf("Enter the size of the tree: ");
