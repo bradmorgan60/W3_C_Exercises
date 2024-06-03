77cc972fecb065981c22449f2433d83ba4bb90a3
Brad Morgan <jbradmorg16@gmail.com>
2024-06-03 16:00:27 +0300

    testing

--- Heap/heap_1.c
+++ Heap/heap_1.c
@@ -7,13 +7,21 @@ struct Heap {
     int size; // display function
 };
 
-void display(struct Heap *heap) {
-    for (int i = 0; i < heap->size; i++) {
-        printf("%d ", heap->arr[i]);
-    }
-    // no memory is being dynamically allocated to the heap
-    printf("\n");
+void swap(int *num1, int *num2) {
+    int temp;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
 }
+
+// void display(struct Heap *heap) {
+//     for (int i = 0; i < heap->size; i++) {
+//         printf("%d ", heap->arr[i]);
+//     }
+//     // no memory is being dynamically allocated to the heap
+//     // we are referencing the struct that does not contain any methods that reference the heap
+//     printf("\n");
+// }
 
 void display_pointer(int size, int *arr) {
     for (int i = 0; i < size; i++) {
@@ -29,18 +37,29 @@ void display_reg(int size, int *arr) {
     printf("\n");
 }
 
+void insert(struct Heap *heap, int key) {
+    if (heap->size >= MAX_HEAP_SIZE) {
+        printf("Heap overflow...\n");
+    }
+
+    heap->arr[heap->size] = key;
+    heap->size++;
+}
+
 int main() {
     struct Heap heap;
     int size, *arr;
+    heap.size = 0;
     printf("Exercise 1: Implement basic heap functionality\n");
 
-    printf("Enter the size of the array: ");
+    printf("Enter the size of the array: (amount of space to allocate): ");
     scanf("%d", &size);
     arr = calloc(size, sizeof(int));
 
     for (int i = 0; i < size; i++) {
         printf("Element-%d: ", i + 1);
         scanf("%d", arr + i);
+        insert(&heap, *(arr + i));
     }
     printf("\n");
 
