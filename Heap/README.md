82bccaeec398f0e595c76ce0bbe09338c3b00e97
Brad Morgan <jbradmorg16@gmail.com>
2024-06-04 11:46:03 +0300

    progressing

---
+++ Heap/README.md
@@ -0,0 +1,14 @@
+This is a markup document that provides an overview of the heap data structure and the reasons why it is important.
+Every data structure / algorithm in computer science exists for a reason, and there must be purpose with every exercise performed.
+
+(in my own words)
+The Heap Data Structure
+The Heap is a region of memory responsible for dynamic memory allocation. In the C programming language, programmers are responsible for allocating, reallocating, deallocating, and freeing memory in their own programs.
+If this is not performed correctly, the program is subject to data leaks and heap overflows...which could crash the program.
+
+The exercises performed in this repository are for my own education on problem solving with the heap, as well as for discovering concepts and tools I can use in future or current projects.
+
+The world is full of possibilities...and the C programming language enables me to dive into the world of engineering and technology. This is the part of my lifelong journey to becoming a great technologist.
+
+
+
--- Heap/heap_1.c
+++ Heap/heap_1.c
@@ -3,88 +3,9 @@
 #define MAX_HEAP_SIZE 100
 
 struct Heap {
-    int arr[MAX_HEAP_SIZE];
-    int size; // display function
+    array[MAX_HEAP_SIZE];
 };
-
-void swap(int *num1, int *num2) {
-    int temp;
-    temp = *num1;
-    *num1 = *num2;
-    *num2 = temp;
-}
-
-// void display(struct Heap *heap) {
-//     for (int i = 0; i < heap->size; i++) {
-//         printf("%d ", heap->arr[i]);
-//     }
-//     // no memory is being dynamically allocated to the heap
-//     // we are referencing the struct that does not contain any methods that reference the heap
-//     printf("\n");
-// }
-
-void display_pointer(int size, int *arr) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
-    }
-    printf("\n");
-}
-
-void display_reg(int size, int *arr) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-void HeapifyUp(struct Heap *heap, int index) {
-    int parent = (index - 1) / 2;
-
-    while (index > 0 && heap->arr[index] > heap->arr[parent]) {
-        swap(&heap->arr[index], &heap->arr[parent]);
-        index = parent; // assigning swapped element with to parent
-        parent = (index - 1) / 2;  // maintain the parent
-    }
-}
-
-void insert(struct Heap *heap, int key) {
-    if (heap->size >= MAX_HEAP_SIZE) {
-        printf("Heap overflow...\n");
-    }
-
-    heap->arr[heap->size] = key;
-    heap->size++;
-
-    HeapifyUp(heap, heap->size - 1);
-}
-
+ 
 int main() {
-    struct Heap heap;
-    int size, *arr;
-    heap.size = 0;
-    printf("Exercise 1: Implement basic heap functionality\n");
-
-    printf("Enter the size of the array: (amount of space to allocate): ");
-    scanf("%d", &size);
-    arr = calloc(size, sizeof(int));
-
-    for (int i = 0; i < size; i++) {
-        printf("Element-%d: ", i + 1);
-        scanf("%d", arr + i);
-        insert(&heap, *(arr + i));
-    }
-    printf("\n");
-
-    // printf("Struct display: ");
-    // display(&heap);
-    // error occurs because we do not have memory dynamically allocating to the heap
-
-    printf("Display via pointer: ");
-    display_pointer(size, arr);
-    // works
-
-    // printf("Regular display: ");
-    // display_reg(size, arr);
-    // works
     return 0;
 }
